d3d027f65bc78fd30e9c4df6fc388b00542408cf
Md Mirza Shihab <msmd2@graduate.utm.my>
2024-09-01 11:09:04 +0600

    Vercel config added

--- src/secret.js
+++ src/secret.js
@@ -2,6 +2,4 @@ require('dotenv').config();
 
 const serverPort = process.env.SERVER_PORT || 3000;
 const MongodbURL = process.env.MONGODB_ATLAS_URL;
-const defaultImagePath = process.env.DEFAULT_USER_IMAGE_PATH || '/public/images/users/avatar.png';
-
-module.exports = {serverPort, MongodbURL, defaultImagePath}
+module.exports = {serverPort, MongodbURL}
